4c3bdbcd192cc6a8615a41d8a3597affabe8ae1a
seharshaikh-bp <seharshaikh@bitspro.com>
2023-08-15 16:35:03 +0500

    TEST COMMIT

--- frontendapp/src/App.js
+++ frontendapp/src/App.js
@@ -1,24 +1,25 @@
-import logo from './logo.svg';
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route
+}from 'react-router-dom'
+import Notes from './pagers/Notes'
+import Note from  './pagers/Note'
+import Layout from './components/Layout'
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Layout> 
+        <Routes>
+          <Route path={'/'} element={<Notes/>} exact />
+          <Route path={'/:id'} element={<Note/>}/>
+        </Routes>
+      </Layout> 
+    </Router>
   );
 }
 
